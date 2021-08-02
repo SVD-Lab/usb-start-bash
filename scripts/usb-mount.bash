c51d2a5cc7a9ce276855ea2764b080b98bd65ed5
Ar-Ray <67567093+Ar-Ray-code@users.noreply.github.com>
2021-08-02 09:18:39 +0900

    Update usb-mount.bash

--- scripts/usb-mount.bash
+++ scripts/usb-mount.bash
@@ -4,5 +4,6 @@ source /home/$USER/.bashrc
 mkdir -p /home/$USER/mnt/
 mount /dev/sda1 /home/$USER/mnt/
 chmod 777 /home/$USER/mnt/
+chown -R $USER:$USER /home/$USER/mnt/
 bash /home/$USER/mnt/usb.bash
 wait
