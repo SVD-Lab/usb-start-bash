cd30842f0c5b703f00af94885b2aa9617be49cc8
Ar-Ray <67567093+Ar-Ray-code@users.noreply.github.com>
2022-07-06 11:43:12 +0900

    Update microxrceagent.bash

--- Copy2USB-Flash-Drive/microxrceagent.bash
+++ Copy2USB-Flash-Drive/microxrceagent.bash
@@ -4,11 +4,5 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 _USER=${SCRIPT_DIR##*home/}
 USER=${_USER%%/*}
 
-/home/$USER/Micro-XRCE-DDS-Agent-dashing/build/MicroXRCEAgent udp4 -p 2000 &
-/home/$USER/Micro-XRCE-DDS-Agent-dashing/build/MicroXRCEAgent udp4 -p 2001 &
-/home/$USER/Micro-XRCE-DDS-Agent-dashing/build/MicroXRCEAgent udp4 -p 2002 &
-/home/$USER/Micro-XRCE-DDS-Agent-dashing/build/MicroXRCEAgent udp4 -p 2003 &
-/home/$USER/Micro-XRCE-DDS-Agent-dashing/build/MicroXRCEAgent udp4 -p 2004 &
-/home/$USER/Micro-XRCE-DDS-Agent-dashing/build/MicroXRCEAgent udp4 -p 2005 &
-
+/home/$USER/Micro-XRCE-DDS-Agent-dashing/build/MicroXRCEAgent udp4 -p 2000
 wait
