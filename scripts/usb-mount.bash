8ff8346129010e8ff44446954ebd8da8ada2f893
Ar-Ray <67567093+Ar-Ray-code@users.noreply.github.com>
2022-10-22 16:33:25 +0900

    update usb-mount.bash

--- scripts/usb-mount.bash
+++ scripts/usb-mount.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
-# RUN as "$USER" (e.g. ubuntu)
-source /home/$USER/.bashrc
+# RUN as "${USER}" (e.g. ubuntu)
+source /home/${USER}/.bashrc
 
-mkdir -p /home/$USER/mnt/
-sudo mount /dev/sda1 /home/$USER/mnt/
-sudo chmod 777 /home/$USER/mnt/
-sudo bash /home/$USER/mnt/usb.bash
+mkdir -p /home/${USER}/mnt/
+sudo mount /dev/sda1 /home/${USER}/mnt/ -o umask=000
+sudo chmod -R 755 /home/${USER}/mnt/
+bash /home/${USER}/mnt/usb.bash
 wait
