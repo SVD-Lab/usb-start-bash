7d0ff62fd0aa07512c650f4c4891d5327d22b85f
Ar-Ray-code <ray255ar@gmail.com>
2023-02-25 11:55:39 +0900

    remove copy2usb

--- Copy2USB-Flash-Drive/microxrceagent.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# COPY TO USB
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
-_USER=${SCRIPT_DIR##*home/}
-USER=${_USER%%/*}
-
-/home/$USER/Micro-XRCE-DDS-Agent-dashing/build/MicroXRCEAgent udp4 -p 2000
-wait
